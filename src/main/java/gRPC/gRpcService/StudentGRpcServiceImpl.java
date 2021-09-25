cee5ddd62eb0672fa34eff9ad088140721d209aa
cycloniccornet <55379577+cycloniccornet@users.noreply.github.com>
2021-09-25 11:44:31 +0200

    Initial commit

---
+++ src/main/java/gRPC/gRpcService/StudentGRpcServiceImpl.java
@@ -0,0 +1,50 @@
+package gRPC.gRpcService;
+
+
+import com.studentAppicationGRpc.stubs.student.StudentGRpcRequest;
+import com.studentAppicationGRpc.stubs.student.StudentGRpcResponse;
+import com.studentAppicationGRpc.stubs.student.StudentGRpcServiceGrpc;
+import gRPC.gRpcDao.StudentGRpcDao;
+
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+
+import gRPC.gRpcDomain.StudentGRpc;
+
+import java.util.NoSuchElementException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StudentGRpcServiceImpl extends StudentGRpcServiceGrpc.StudentGRpcServiceImplBase {
+
+    private static final Logger logger = Logger.getLogger(StudentGRpcServiceImpl.class.getName());
+
+    //Data access object that fetches data from the db and returns it
+    private StudentGRpcDao studentGRpcDao = new StudentGRpcDao();
+
+    @Override
+    public void getStudentInfo(StudentGRpcRequest request, StreamObserver<StudentGRpcResponse> responseObserver) {
+        int studentId = (int) request.getStudentId();// the student ID should be passed with the request message
+
+        try{
+            StudentGRpc studentGRpc = studentGRpcDao.findById(studentId);
+
+            System.out.println("Student object "+ studentGRpc);
+            StudentGRpcResponse studentGRpcResponse = StudentGRpcResponse.newBuilder()
+                    .setStudentId(studentId)
+                    .setStudentName(studentGRpc.getStudentName())
+                    .setStudentMail(studentGRpc.getStudentMail())
+                    .build();
+
+            System.out.println("Student repsone object: \n"+ studentGRpcResponse);
+
+            responseObserver.onNext(studentGRpcResponse);
+            responseObserver.onCompleted();
+        }catch (NoSuchElementException e){
+            logger.log(Level.SEVERE, "NO STUDENT FOUND WITH THE STUDENT ID: "+studentId);
+
+            // If some error occurs we sent an error with the following status which is not_found
+            responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
+        }
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO studentGRpc (studentId, studentName, studentMail) VALUES (0, 'Cartman', 'cartman@mail.dk');
+INSERT INTO studentGRpc (studentId, studentName, studentMail) VALUES (1, 'Kenny', 'kenny@mail.dk');
+INSERT INTO studentGRpc (studentId, studentName, studentMail) VALUES (2, 'Stan', 'stan@mail.dk');
+INSERT INTO studentGRpc (studentId, studentName, studentMail) VALUES (3, 'Kyle', 'kyle@mail.wld');
+INSERT INTO studentGRpc (studentId, studentName, studentMail) VALUES (4, 'Chef', 'chef@bobov.dol');
+INSERT INTO studentGRpc (studentId, studentName, studentMail) VALUES (5, 'Hanky the Christmas Poo', 'poo@mail.us');
